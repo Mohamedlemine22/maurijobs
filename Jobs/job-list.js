7844bb2097372fff884024fb8c25b2b56cbe4f6a
Mohamed Lemine Mihi <85792588+Mohamedlemine22@users.noreply.github.com>
2025-04-19 01:03:44 +0300

    Add files via upload

---
+++ Jobs/job-list.js
@@ -0,0 +1,87 @@
+const jCategory = [
+    {
+        index: 0,
+        image: "../images/google.png",
+        title: "Web Developer",
+        rate: "$3000-4500/m",
+        av: "Full Time",
+        companyName: "Google",
+        location: "USA",
+        vacancy: "01",
+        hours: "50h / Week",
+        description: "Front-End Software Engineer (Hybrid ) Google Technologies, a leader in vehicle route optimization software technology, is seeking a front-end software engineer for their development team in Melville, NY. This role will be responsible for designing, coding, and supporting multiple websites that must feature both user-friendly design and clear navigation, from layout to function.The typical base pay range for this position at the start of employment is expected to be between $100,000-$110,000 per year. RouteSmart Technologies, Inc. has different base pay ranges for different work locations within the US and base pay will vary based on experience.This position is hybrid or remote; however, we are currently seeking only candidates who are domiciled in the following states: Maryland; New York; Pennsylvania; Arizona; California; Colorado; Florida; Idaho; Illinois; Indiana; Minnesota; New Jersey; North Carolina; Oregon; South Carolina; Washington Essential Duties & Responsibilities • Create site layout/user interface by using standard HTML/CSS practices • Write well designed, testable, and efficient code by using best software development practices • Demonstrate the ability to identify and fix errors in code • Integrate data from various back-end services and databases and consult other web designers to match visual design intent • Strong understanding of UI, cross-browser compatibility, general web functions and ECMA Script standards • Deep expertise and hands on experience creating Web Applications using one or more JavaScript frameworks, such as ReactJS and Material UI • Understanding of web application security, session management, and code development best practices • Deep functional knowledge or hands on experience with making calls to Web Services • Working knowledge of GIS and similar mapping services extremely useful Education & Experience • Bachelor’s Degree in Computer Science, Information Systems, Business Technology, or a related field • 3+ years minimum experience as a UX designer and/or web developer/front-end software developer preferred • Experience in planning and delivering software platforms used across multiple products and organizational units",
+        workplace: "office",
+        education: "Bachelor degree in Computer Science Or Related field",
+        experience: "2 to 4 Year(s)",
+    },
+
+    {
+        index: 1,
+        image: "../images/uber.png",
+        title: "Data Analysis",
+        rate: "$3000-4500/m",
+        av: "Freelancer",
+        companyName: "Uber",
+        location: "Nouakchott",
+        vacancy: "03",
+        hours: "40h / Week",
+        description: "Data Analysis (remote ) UBER, a leader in vehicle route optimization software technology, is seeking a Digital Marketing for their development team in Melville, NY. This role will be responsible for designing, coding, and supporting multiple websites that must feature both user-friendly design and clear navigation, from layout to function.The typical base pay range for this position at the start of employment is expected to be between $100,000-$110,000 per year. RouteSmart Technologies, Inc. has different base pay ranges for different work locations within the US and base pay will vary based on experience.This position is hybrid or remote; however, we are currently seeking only candidates who are domiciled in the following states: Maryland; New York; Pennsylvania; Arizona; California; Colorado; Florida; Idaho; Illinois; Indiana; Minnesota; New Jersey; North Carolina; Oregon; South Carolina; Washington Essential Duties & Responsibilities • Create site layout/user interface by using standard HTML/CSS practices • Write well designed, testable, and efficient code by using best software development practices • Demonstrate the ability to identify and fix errors in code • Integrate data from various back-end services and databases and consult other web designers to match visual design intent • Strong understanding of UI, cross-browser compatibility, general web functions and ECMA Script standards • Deep expertise and hands on experience creating Web Applications using one or more JavaScript frameworks, such as ReactJS and Material UI • Understanding of web application security, session management, and code development best practices • Deep functional knowledge or hands on experience with making calls to Web Services • Working knowledge of GIS and similar mapping services extremely useful Education & Experience • Bachelor’s Degree in Computer Science, Information Systems, Business Technology, or a related field • 3+ years minimum experience as a UX designer and/or web developer/front-end software developer preferred • Experience in planning and delivering software platforms used across multiple products and organizational units",
+        workplace: "remote",
+        education: "Bachelor degree in Computer Science Or Related field",
+        experience: "3 to 4 Year(s)",
+    },
+
+    {
+        index: 2,
+        image: "../images/Linkedin.png",
+        title: "Product Manager",
+        rate: "$4700-5000/m",
+        av: "Part Time",
+        companyName: "Linkedin",
+        location: "USA",
+        vacancy: "02",
+        hours: "30h / Week",
+        description: "Product Manager (Hybrid ) Linkedin, a leader in vehicle route optimization software technology, is seeking a Product Manager for their development team in Melville, NY. This role will be responsible for designing, coding, and supporting multiple websites that must feature both user-friendly design and clear navigation, from layout to function.The typical base pay range for this position at the start of employment is expected to be between $100,000-$110,000 per year. RouteSmart Technologies, Inc. has different base pay ranges for different work locations within the US and base pay will vary based on experience.This position is hybrid or remote; however, we are currently seeking only candidates who are domiciled in the following states: Maryland; New York; Pennsylvania; Arizona; California; Colorado; Florida; Idaho; Illinois; Indiana; Minnesota; New Jersey; North Carolina; Oregon; South Carolina; Washington Essential Duties & Responsibilities • Create site layout/user interface by using standard HTML/CSS practices • Write well designed, testable, and efficient code by using best software development practices • Demonstrate the ability to identify and fix errors in code • Integrate data from various back-end services and databases and consult other web designers to match visual design intent • Strong understanding of UI, cross-browser compatibility, general web functions and ECMA Script standards • Deep expertise and hands on experience creating Web Applications using one or more JavaScript frameworks, such as ReactJS and Material UI • Understanding of web application security, session management, and code development best practices • Deep functional knowledge or hands on experience with making calls to Web Services • Working knowledge of GIS and similar mapping services extremely useful Education & Experience • Bachelor’s Degree in Computer Science, Information Systems, Business Technology, or a related field • 3+ years minimum experience as a UX designer and/or web developer/front-end software developer preferred • Experience in planning and delivering software platforms used across multiple products and organizational units",
+        workplace: "office",
+        education: "Bachelor degree in Computer science Or Related field",
+        experience: "3 to 4 Year(s)",
+    },
+
+    {
+        index: 3,
+        image: "../images/facebook.png",
+        title: "Digital Marketing",
+        rate: "$900-1200/m",
+        av: "Full Time",
+        companyName: "Facebook",
+        location: "Nouakchott",
+        vacancy: "02",
+        hours: "40h / Week",
+        description: "Digital Marketing (remote ) ,Facebook a leader in vehicle route optimization software technology, is seeking a Product Manager for their development team in Melville, NY. This role will be responsible for designing, coding, and supporting multiple websites that must feature both user-friendly design and clear navigation, from layout to function.The typical base pay range for this position at the start of employment is expected to be between $100,000-$110,000 per year. RouteSmart Technologies, Inc. has different base pay ranges for different work locations within the US and base pay will vary based on experience.This position is hybrid or remote; however, we are currently seeking only candidates who are domiciled in the following states: Maryland; New York; Pennsylvania; Arizona; California; Colorado; Florida; Idaho; Illinois; Indiana; Minnesota; New Jersey; North Carolina; Oregon; South Carolina; Washington Essential Duties & Responsibilities • Create site layout/user interface by using standard HTML/CSS practices • Write well designed, testable, and efficient code by using best software development practices • Demonstrate the ability to identify and fix errors in code • Integrate data from various back-end services and databases and consult other web designers to match visual design intent • Strong understanding of UI, cross-browser compatibility, general web functions and ECMA Script standards • Deep expertise and hands on experience creating Web Applications using one or more JavaScript frameworks, such as ReactJS and Material UI • Understanding of web application security, session management, and code development best practices • Deep functional knowledge or hands on experience with making calls to Web Services • Working knowledge of GIS and similar mapping services extremely useful Education & Experience • Bachelor’s Degree in Computer Science, Information Systems, Business Technology, or a related field • 3+ years minimum experience as a UX designer and/or web developer/front-end software developer preferred • Experience in planning and delivering software platforms used across multiple products and organizational units",
+        workplace: "remote",
+        education: "Bachelor degree in Comerse faculty Or Related field",
+        experience: "3 to 4 Year(s)",
+    },
+
+    {
+        index: 4,
+        image: "../images/yahoo.png",
+        title: "UI & UX",
+        rate: "$4700-5000/m",
+        av: "Part Time",
+        companyName: "Yaho",
+        location: "USA",
+        vacancy: "02",
+        hours: "20h / Week",
+        description: "UI & UX (remote ) Linkedin, a leader in vehicle route optimization software technology, is seeking a Product Manager for their development team in Melville, NY. This role will be responsible for designing, coding, and supporting multiple websites that must feature both user-friendly design and clear navigation, from layout to function.The typical base pay range for this position at the start of employment is expected to be between $100,000-$110,000 per year. RouteSmart Technologies, Inc. has different base pay ranges for different work locations within the US and base pay will vary based on experience.This position is hybrid or remote; however, we are currently seeking only candidates who are domiciled in the following states: Maryland; New York; Pennsylvania; Arizona; California; Colorado; Florida; Idaho; Illinois; Indiana; Minnesota; New Jersey; North Carolina; Oregon; South Carolina; Washington Essential Duties & Responsibilities • Create site layout/user interface by using standard HTML/CSS practices • Write well designed, testable, and efficient code by using best software development practices • Demonstrate the ability to identify and fix errors in code • Integrate data from various back-end services and databases and consult other web designers to match visual design intent • Strong understanding of UI, cross-browser compatibility, general web functions and ECMA Script standards • Deep expertise and hands on experience creating Web Applications using one or more JavaScript frameworks, such as ReactJS and Material UI • Understanding of web application security, session management, and code development best practices • Deep functional knowledge or hands on experience with making calls to Web Services • Working knowledge of GIS and similar mapping services extremely useful Education & Experience • Bachelor’s Degree in Computer Science, Information Systems, Business Technology, or a related field • 3+ years minimum experience as a UX designer and/or web developer/front-end software developer preferred • Experience in planning and delivering software platforms used across multiple products and organizational units",
+        workplace: "remote",
+        education: "Bachelor degree in any field",
+        experience: "3 to 4 Year(s)",
+    },
+
+    
+    
+
+   
+    
+];
